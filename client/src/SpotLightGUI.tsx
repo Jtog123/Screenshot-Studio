d13c6c5e208f39409f3c1d82f4b5cb70043c3473
James Togher <jamestogher567@gmail.com>
2026-03-09 17:36:13 -0400

    x

--- client/src/SpotLightGUI.tsx
+++ client/src/SpotLightGUI.tsx
@@ -204,7 +204,7 @@ export default function SpotLightGUI({_lightID, _lightManager} : SpotLightGUIPro
         {
             <div style={{
                 transform: `translate(${guiPosition.x}px, ${guiPosition.y}px)`}}  
-                className="absolute rounded-xl right-[800px] top-[200px] min-w-[300px] min-h-[150px] max-w-[450px] overflow-auto bg-cream-vanilla pb-3 z-50 backdrop-blur-md border border-stone-600 shadow-[0_0_20px_rgba(120,113,108,0.3)] ring-1 ring-stone-700/50"
+                className="absolute rounded-xl right-[800px] top-[200px] min-w-[300px] min-h-[150px] max-w-[450px] overflow-auto bg-cream-vanilla pb-3 z-50 backdrop-blur-md border border-espresso shadow-[0_0_20px_rgba(120,113,108,0.3)] ring-1 ring-stone-700/50"
                 >
                 <div onMouseDown={handleMouseDown} className="sticky top-0 flex items-center justify-between  cursor-move bg-cream-golden w-full py-2 px-4 z-10">
                     <div className="titlebox ">
