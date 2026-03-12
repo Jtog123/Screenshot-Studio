77a510e99b3250cbfd02a0a33c23052b5f767798
James Togher <jamestogher567@gmail.com>
2026-03-11 21:48:55 -0400

    working on homepage

--- client/src/FooterMenu.tsx
+++ client/src/FooterMenu.tsx
@@ -57,7 +57,7 @@ export default function FooterMenu() {
 
     return (
         <>
-            <div className="absolute p-1.5 z-dropdown bg-bg-000 border border-border-200 backdrop-blur-xl rounded-xl min-w-[16rem] text-text-300 shadow-lg max-h-[24rem] overflow-y-auto mx-0.5 bottom-15 right-0">
+            <div className="absolute p-1.5 z-dropdown bg-bg-000 border border-border-200 backdrop-blur-xl rounded-xl min-w-[16rem] text-text-300 shadow-lg max-h-[24rem] overflow-y-auto mx-0.5 bottom-15 right-4">
             
                 {/* Email header */}
                 <div className="text-text-500 pt-1 px-2 pb-2 truncate">
@@ -65,7 +65,7 @@ export default function FooterMenu() {
                 </div>
 
                 {/* Menu item */}
-                <a href="/settings" className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer">
+                <a href="/settings" className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer hover:bg-crust-light/50">
                     {/* Icon here */}
                     <span>Settings</span>
                 </a>
@@ -74,7 +74,7 @@ export default function FooterMenu() {
                 <div className="h-[0.5px] bg-border-300 my-1.5 mx-2" />
 
                 {/* More menu items */}
-                <a href="/upgrade" className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer">
+                <a href="/upgrade" className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer hover:bg-crust-light/50">
                     <span>Upgrade plan</span>
                 </a>
 
@@ -83,7 +83,7 @@ export default function FooterMenu() {
                 <div className="h-[0.5px] bg-border-300 my-1.5 mx-2" />
 
                 {/* Logout */}
-                <a onClick={handleUserLogout} className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer">
+                <a onClick={handleUserLogout} className="flex items-center gap-2 min-h-8 px-2 py-1.5 rounded-lg hover:bg-bg-200 cursor-pointer hover:bg-crust-light/50">
                     <span>Log out</span>
                 </a>
 
--- client/src/Routes/public/HomePage.tsx
+++ client/src/Routes/public/HomePage.tsx
@@ -1,11 +1,79 @@
 import NavigationBar from "../../NavigationBar"
+import { useState, useRef, useEffect } from "react";
+import * as THREE from 'three';
+
+
 export default function HomePage() {
+
+    const fov : number = 60; //75
+    const aspect : number = (window.innerWidth /2) / window.innerHeight;
+    const near : number = 0.1;
+    const far : number = 10000;
+
+    const mountRef = useRef<HTMLDivElement | null>(null);
+
+
+    useEffect(() => {
+        if(!mountRef.current) return;
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth / 2, window.innerHeight);
+        
+
+        renderer.domElement.style.position = "relative";
+        renderer.domElement.style.display = "block";
+
+
+        mountRef.current.appendChild(renderer.domElement);
+
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+
+        //run function
+        const run = () => {
+            requestAnimationFrame(() => run());
+            cube.rotation.x += 0.01;  // Add rotation to see it's working
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        }
+
+        //run the animation loop
+        run();
+
+        //clean up the ref
+        return () => {
+            if(mountRef.current) {
+                mountRef.current?.removeChild(renderer.domElement);
+            }
+
+            renderer.dispose();
+            geometry.dispose();
+            material.dispose();
+        };
+
+
+    },[])
+
+
     return(
         <>
         <div>
             <NavigationBar/>
-            <div className="fixed flex w-[100%] h-[100%] bg-stone-700 z-20">
-                testing page
+            <div className="fixed flex w-[100%] h-[100%] z-20 bg-stone-700">
+                
+                <div className="leftSide bg-red-500 h-[100%] w-[50%]">
+                </div>
+
+                <div ref={mountRef} className="rightSide bg-yellow-600 h-[100%] w-[50%]">
+                    {/*Phone Demo here */}
+               
+                </div>
 
             </div>
         </div>
